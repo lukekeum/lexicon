b9724f4ba1077059916547ddcf31446a0ca94e5e
lukekeum <lukekeum11@gmail.com>
2026-01-12 06:23:14 +0900

    fix: update exports

--- decorators/index.ts
+++ decorators/index.ts
@@ -2,3 +2,7 @@ export * from './command.decorator';
 export * from './arg.decorator';
 export * from './ctx.decorator';
 export * from './execute.decorator';
+export * from './subCommand.decorator';
+export * from './option.decorator';
+export * from './choice.decorator';
+export * from './autoComplete.decorator';
